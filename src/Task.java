1fd9a59c0c0c64076608cf27c4f42cc3429d294e
JWilliams-Keyin <Jack.Williams@keyin.com>
2025-06-18 18:39:09 -0230

    Added code for Task.java POJO

--- src/Task.java
+++ src/Task.java
@@ -1,2 +1,48 @@
 public class Task {
+
+    // Attributes
+
+    public String taskDescription;
+    public boolean taskStatus;
+
+    // Constructor
+
+    public Task(String taskDescription) {
+        this.taskDescription = taskDescription;
+        this.taskStatus = false;
+    }
+
+    // Getters & Setters
+
+    public String getTaskDescription() {
+        return taskDescription;
+    }
+
+    public void setTaskDescription(String taskDescription) {
+        this.taskDescription = taskDescription;
+    }
+
+    public boolean getTaskStatus() {
+        return taskStatus;
+    }
+
+    public void setTaskStatus(boolean taskStatus) {
+        this.taskStatus = taskStatus;
+    }
+
+    // Methods
+
+    // This method converts the boolean version of taskStatus to a "Complete" or "Pending" status
+    public String convertBooleanStatusToString() {
+        if (this.taskStatus) {
+            return "Complete";
+        } else {
+            return "Pending";
+        }
+    }
+
+    public String toString() {
+        return "Task: " + taskDescription + "\n" +
+                "Status: " + convertBooleanStatusToString();
+    }
 }
